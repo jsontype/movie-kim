4ec01492ecfc52a375be8f96b4783ff01196e0ee
netkro <kroging@gmail.com>
2024-11-25 01:34:58 +0900

    feat: api module생성

---
+++ src/api/Index.tsx
@@ -0,0 +1,34 @@
+import { apiEndpoint } from '@/types/apiEndpoint';
+import { listMovies } from '@/types/listMovies';
+
+const API_URL = 'https://yts.mx/api/v2/';
+
+const fetchAPI = async (endPoint: apiEndpoint, params = {}) => {
+  try {
+    const query = new URLSearchParams(params).toString();
+    const res = await fetch(`${API_URL}${endPoint}?${query}`);
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('failed to fetch', error);
+    throw error;
+  }
+};
+
+export const listMoviesApi = (params = {}): Promise<listMovies> => {
+  return fetchAPI('list_movies.json', params);
+};
+export const movieDetailsApi = (params = {}): Promise<any> => {
+  return fetchAPI('movie_details.json', params);
+};
+export const movieParentalGuidesApi = (params = {}): Promise<any> => {
+  return fetchAPI('movie_parental_guides.json', params);
+};
+export const movieSuggestionsApi = (params = {}): Promise<any> => {
+  return fetchAPI('movie_suggestions.json', params);
+};
