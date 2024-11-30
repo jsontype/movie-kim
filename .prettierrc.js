d1f708a441f67e5ea7913fdb92badd55e3ee78be
jsontype <jsontyper@gmail.com>
2024-11-30 14:59:58 +0900

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.export = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  bracketSpacing: true, 
+  jsxBracketSameLine: true,
+  trailingComma: 'all',
+}
